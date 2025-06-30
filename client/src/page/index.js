8dfa48f45a257ab2f42f923a997e79adc20a5a66
Asthaa31 <149063994+Asthaa31@users.noreply.github.com>
2025-06-30 19:09:13 +0530

    Update

--- client/src/page/index.js
+++ client/src/page/index.js
@@ -3,3 +3,4 @@ export { default as Error } from './error'
 export { default as Login } from './login'
 export { default as Signup } from './signup'
 export { default as Forgot } from './forgot'
+//final
